25998c3b99a2cde71bf0251faf2df5281faf02a4
mohd furkan <76490756+mohdfurkan01@users.noreply.github.com>
2024-08-31 20:43:55 +0000

    some operation on str and num

---
+++ js_container/04_operations.js
@@ -0,0 +1,18 @@
+let value = 5;
+let negativeValue = -value;
+console.log(negativeValue);
+
+
+let str1 = "hello"
+let str2 = " Ahsan" //to get space
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2");  //12
+console.log("1" +2 +2); //122
+console.log(1 +2 +"2") //32
+
+console.log(+true); //1
+console.log(+""); //false
+
