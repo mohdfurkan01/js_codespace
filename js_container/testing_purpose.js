7e3a2b29e4a82cb137982f637f19be3900ca2263
mohd furkan <76490756+mohdfurkan01@users.noreply.github.com>
2024-08-31 20:27:15 +0000

    Adding some steps to setup codespace

---
+++ js_container/testing_purpose.js
@@ -0,0 +1,5 @@
+let step_to_configure_codeSpace = "firstly create a new repo then go code section and there are two option one is Local and other is codespaces choose codespace by showing the message create codespace on master now open this and you will see an editor will open then go to command pallete and search container then click Add Dev Container Configuration Files after this again search for Node.js and JavaScript and select by default version that's it it will add a config file "
+
+console.log(step_to_configure_codeSpace);
+
+console.log("trying to configure container")
