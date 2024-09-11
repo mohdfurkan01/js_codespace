991c0d8f6b701ad0d6f9f55f77a843dd981951d9
mohd furkan <76490756+mohdfurkan01@users.noreply.github.com>
2024-09-11 20:17:49 +0000

    Execution context

---
+++ 03_basics/context_execution.js
@@ -0,0 +1,63 @@
+//                          JavaScript Execution Context:
+// In JavaScript, any code is executed in two phases, which are managed by the execution context. These phases are:
+
+// Memory Creation Phase: During this phase, all variables and functions are allocated memory. Variables are initialized with undefined, while functions are stored with their full definitions.
+
+val1 = undefined
+val2 = undefined
+addNum = definition
+
+let val1 = 10;
+let val2 = 5;
+function addNum(num1, num2) {
+  let total = num1 + num2;
+  return total;
+}
+let result1 = addNum(val1, val2);
+let result2 = addNum(10, 2);
+console.log(result1); //15
+console.log(result2); //12
+
+// Execution Phase: In this phase, the actual execution of the code takes place. Variables get their actual values, and functions are invoked.
+
+
+//                       Types of Execution Contexts:
+//1. Global Execution Context:
+
+// When the code starts, it first enters the Global Execution Context. This context handles all global variables and functions.
+// The global execution context is stored in the this keyword, which typically refers to the global object (e.g., window in browsers).
+
+
+//2. Function Execution Context:
+
+// Every time a function is called, a new execution context is created for that function. Each function call has its own execution context.
+// Like the global context, the function execution context has:
+// A Memory Creation Phase where variables and parameters are initialized to undefined.
+// An Execution Phase where actual values are assigned, and the function’s logic is executed.
+
+//1. Memory Creation Phase (Global Execution Context):
+val1 = undefined
+val2 = undefined
+addNum = function (definition){},
+result1 = undefined
+result2 = undefined
+
+//2. Execution Phase (Global Execution Context):
+val1 = 10
+val2 = 5
+//When result1 = addNum(val1, val2) is called, a new function execution context is created:
+//Memory Creation Phase (for addNum):
+num1 = undefined
+num2 = undefined
+total = undefined
+
+//Execution Phase (for addNum):
+num1 = 10
+num2 = 5
+total = num1 + num2 //= 15
+return 15
+//so result1 = 15
+
+// Every time a function is called, a new execution context is created, which is cleared after the function execution. The code first goes through a memory creation phase where everything is undefined, and then the execution phase assigns actual values and executes the logic.
+
+
