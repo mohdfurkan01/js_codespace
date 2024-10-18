c64b34add769649ba0755832d1145ce042ab27b3
Nawab_Saab <furkanchisti786@gmail.com>
2024-10-18 22:03:10 +0530

    static method

--- 12_classes_oops/staticprop.js
+++ 12_classes_oops/staticprop.js
@@ -1,13 +1,16 @@
 //prop=> properties
+//? Static methods are functions that belong to the class itself, not to any instance of the class. You call them using the class name, not on an object created from the class.
 class User {
   constructor(username) {
     this.username = username;
   }
 
+  // Instance method
   logMe() {
     console.log(`USER name is ${this.username}`);
   }
 
+  // Static method
   //createId() { //it has access of ahsan obj
   static createId() {
     //but it doesn't allow the access
@@ -17,8 +20,10 @@ class User {
   }
 }
 
+// Creating an instance
 const ahsan = new User("Ahsan");
-//console.log(ahsan.createId());
+// Trying to call static method from an instance would fail
+//console.log(ahsan.createId()); // Error! This won't work
 
 class Teacher extends User {
   constructor(username, email) {
