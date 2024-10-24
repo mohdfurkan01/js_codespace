ee5b9b093aa0bf011ec2998b54906f88f17d4df0
Nawab_Saab <furkanchisti786@gmail.com>
2024-10-25 03:32:54 +0530

    about variable in js

--- 01_baiscs/01_variable.js
+++ 01_baiscs/01_variable.js
@@ -1,15 +1,15 @@
 const companyId = 12342;
 let companyEmail = "sdegoogle@gmail.com";
-var companyPassword = "78787"
-companyCity =  "New Delhi"; //but not a good way
-let companyState ;
+var companyPassword = "78787";
+companyCity = "New Delhi"; //but not a good way
+let companyState;
 //when a variable is declared and not assigned any value it will give us "undefined"
 
-//companyId = 555 //can not changed 
+//companyId = 555 //can not changed never
 
 companyEmail = "dev123@gmail.com";
 companyPassword = "33334";
-companyCity = "Hydrabad"
+companyCity = "Hydrabad";
 
 console.log(companyId); //only 1 statement can write
 
@@ -18,4 +18,10 @@ Prefer not to use var
 because of issue in block scope and functional scope
 */
 
-console.table([companyId, companyEmail, companyPassword, companyCity, companyState]);
+console.table([
+  companyId,
+  companyEmail,
+  companyPassword,
+  companyCity,
+  companyState,
+]);
