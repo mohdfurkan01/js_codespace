7b68b8e44094455ae5d737887fe0c58cc1f8d01e
Nawab_Saab <furkanchisti786@gmail.com>
2024-10-27 02:35:48 +0530

    about holey array

--- 15-practice/test.js
+++ 15-practice/test.js
@@ -12,7 +12,7 @@ const myArr = [];
 //Packed element
 //Double (float, string, function)
 
-const arrTwo = [1, 2, 3, 4, 5];
+const arrTwo = [1, 2, 3, 4, 5, 6];
 //type: PACKED_SMI_ELEMENTS (best optimize array only for numbers not including any other type like decimal or other)
 arrTwo.push(6.0);
 //PACKED_DOUBLE_ELEMENTS
