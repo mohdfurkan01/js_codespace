06610f354eade5c9b271f7549d8c73056593d87e
Nawab_Saab <furkanchisti786@gmail.com>
2024-11-04 03:31:14 +0530

    control flow

--- 04_Control_Flow/01_One.js
+++ 04_Control_Flow/01_One.js
@@ -4,18 +4,17 @@ const isUserLoggedIn = false;
 const temperature = 41;
 
 if (temperature === 40) {
-    console.log("less than 50");
-    
-}else{
-    console.log("greater than 50"); 
+  console.log("less than 50");
+} else {
+  console.log("greater than 50");
 }
 
 //<,>, <=, >=, ==, !=, ===, !==
 
 const score = 200;
-if(score >100){
-   const power = "fly"
-   console.log(`user's power is: ${power}`) 
+if (score > 100) {
+  const power = "fly";
+  console.log(`user's power is: ${power}`);
 }
 
 //console.log(`user's power is: ${power}`) //ReferenceError: power is not defined
@@ -24,10 +23,9 @@ const userLoggedIn = true;
 const debitCard = true;
 
 if (userLoggedIn && debitCard) {
-    //if (userLoggedIn && debitCard && 2===2) {
+  //if (userLoggedIn && debitCard && 2===2) {
   // we can check multiple conditions using logical operators.
-    console.log("Allow to buy BMW");
-    
+  console.log("Allow to buy BMW Car");
 }
 // (AND operator) && requires both conditions to be true.
 // (OR operator) || requires at least one condition must be true.
@@ -35,6 +33,5 @@ if (userLoggedIn && debitCard) {
 const loggedInFromGoogle = false;
 const loggedInFromEmail = true;
 if (loggedInFromEmail || loggedInFromGoogle) {
-    console.log("User can log in");
-    
+  console.log("User can log in");
 }
