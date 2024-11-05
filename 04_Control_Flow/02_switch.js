d5891eb31ed32d346f54c335e5aae8d7df55aa7b
Nawab_Saab <furkanchisti786@gmail.com>
2024-11-05 18:06:30 +0530

    switch cases

--- 04_Control_Flow/02_switch.js
+++ 04_Control_Flow/02_switch.js
@@ -1,57 +1,56 @@
 //syntax
 let key = true;
-let value = 9;
+let value = 6;
 switch (key) {
-    case value:
-        
-        break;
+  case value:
+    break;
 
-    default:
-        break;
+  default:
+    break;
 }
 //Each case checks if its value matches the key.
 //If no match is found, the default block runs (if provided)
-//const month = "march" 
+//const month = "march"
 const month = 3;
-switch (month){
-    case 1:
-    console.log("Jan month")
-     break;
-    case 2:
-    console.log("Feb month")
-     break;
-     //case "march"
-    case 3:
-    console.log("March month")
-     break;
-    case 4:
-    console.log("April month")
-     break;
-    case 5:
-    console.log("May month")
-     break;
-    case 6:
-    console.log("June month")
-     break;
-    case 7:
-    console.log("July month")
-     break;
-    case 8:
-    console.log("Aug month")
-     break;
-    case 9:
-    console.log("Sep month")
-     break;
-    case 10:
-    console.log("Oct month")
-     break;
-    case 11:
-    console.log("Nov month")
-     break;
-    case 12:
-    console.log("Dec month")
-     break;
-     default:
-        console.log("Entered wrong month number")
-        break
+switch (month) {
+  case 1:
+    console.log("Jan month");
+    break;
+  case 2:
+    console.log("Feb month");
+    break;
+  //case "march"
+  case 3:
+    console.log("March month");
+    break;
+  case 4:
+    console.log("April month");
+    break;
+  case 5:
+    console.log("May month");
+    break;
+  case 6:
+    console.log("June month");
+    break;
+  case 7:
+    console.log("July month");
+    break;
+  case 8:
+    console.log("Aug month");
+    break;
+  case 9:
+    console.log("Sep month");
+    break;
+  case 10:
+    console.log("Oct month");
+    break;
+  case 11:
+    console.log("Nov month");
+    break;
+  case 12:
+    console.log("Dec month");
+    break;
+  default:
+    console.log("Entered wrong month number");
+    break;
 }
