89f6910dc5a1d8d92a8657f750a4b520072c4d28
Nawab_Saab <furkanchisti786@gmail.com>
2025-12-18 09:49:11 +0530

    fixed a minor bug means just a typo

--- 01_baiscs/04_operations.js
+++ 01_baiscs/04_operations.js
@@ -1,18 +1,16 @@
 let value = 5;
 let negativeValue = -value;
-console.log(negativeValue);
+console.log(negativeValue); //output: -5
 
-
-let str1 = "hello"
-let str2 = " Ahsan" //to get space
+let str1 = "hello";
+let str2 = " Ahsan"; //to get space
 let str3 = str1 + str2;
 console.log(str3);
 
 console.log("1" + 2); //12
-console.log(1 + "2");  //12
-console.log("1" +2 +2); //122
-console.log(1 +2 +"2") //32
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
 
 console.log(+true); //1
 console.log(+""); //false
-
